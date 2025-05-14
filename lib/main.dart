c8a9404e0824238f6734ee2bc46db93b465b3455
ydmins <dr.ydmins@gmail.com>
2025-05-14 19:31:43 +0900

    feature: add init screen and route it

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,23 @@
+import 'package:flutter/material.dart';
 
+import 'screens/init_screen.dart';
+
+void main() {
+  runApp(UebungMachtMeisterApp());
+}
+
+class UebungMachtMeisterApp extends StatelessWidget {
+  const UebungMachtMeisterApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Uebung Macht Meister',
+        theme: ThemeData(primarySwatch: Colors.red),
+        initialRoute: '/',
+        routes: {
+          '/': (context) => InitScreen(),
+        });
+  }
+}
